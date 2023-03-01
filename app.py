cb779e0357de095fea662b963814fa5b21d4f876
Nirmala Koshiga <101269369+NIRMALAKOSHIGA@users.noreply.github.com>
2023-03-01 21:12:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+st.title("Machine Learning Model:Hert Disease Prediction")
+dataframe = pd.read_csv("heart.csv")
+st.write(dataframe)
+from PIL import Image
+st.subheader("Disease of heart")
+
+image = Image.open('pic.png')
+st.image(image, caption="HEART DISEASE PREDICTION")
+
